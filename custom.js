ca2c8283d17bbc3194cbbc3acdd60c9edf4fb99f
antobecagaga <antonelaxgg@outlook.com>
2024-11-30 16:03:04 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -9,10 +9,23 @@
     });
 
     // NAVBAR
-  $(".navbar").headroom();
+    $(".navbar").headroom();
 
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
+    });
+
+    $('.slick-slideshow').slick({
+      autoplay: true,
+      infinite: true,
+      arrows: false,
+      fade: true,
+      dots: true,
+    });
+
+    $('.slick-testimonial').slick({
+      arrows: false,
+      dots: true,
     });
     
   })(window.jQuery);
